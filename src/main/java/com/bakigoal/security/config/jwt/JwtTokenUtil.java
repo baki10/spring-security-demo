fbb88e6d412aa0108820e1ac863041345ba31f7d
Ilmir Bakirov <bakirov@xrm.ru>
2020-09-09 22:54:40 +0500

    use application yaml

--- src/main/java/com/bakigoal/security/config/jwt/JwtTokenUtil.java
+++ src/main/java/com/bakigoal/security/config/jwt/JwtTokenUtil.java
@@ -19,7 +19,7 @@ public class JwtTokenUtil implements Serializable {
 
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-    @Value("${jwt.secret}")
+    @Value("${app.security.jwt.secret}")
     private String secret;
 
     public String getUsernameFromToken(String token) {
