0b3ddfb74bedbf04dcfdfebb3877a00b6ff7d990
Ilmir Bakirov <bakirov@xrm.ru>
2020-12-11 21:10:52 +0500

    log user info

--- keycloak-demo/src/main/java/com/bakigoal/keycloakdemo/rest/WorkoutController.java
+++ keycloak-demo/src/main/java/com/bakigoal/keycloakdemo/rest/WorkoutController.java
@@ -3,13 +3,18 @@ package com.bakigoal.keycloakdemo.rest;
 import com.bakigoal.keycloakdemo.model.Workout;
 import com.bakigoal.keycloakdemo.service.WorkoutService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/workout")
 @AllArgsConstructor
+@Slf4j
 public class WorkoutController {
 
     private final WorkoutService workoutService;
@@ -20,8 +25,16 @@ public class WorkoutController {
     }
 
     @GetMapping("/")
-    public List<Workout> workouts() {
+    public List<Workout> workouts(OAuth2Authentication authentication) {
+        logUser(authentication);
         return workoutService.findWorkouts();
+    }
+
+    private void logUser(OAuth2Authentication authentication) {
+        Collection<GrantedAuthority> authorities = authentication.getAuthorities();
+        String name = authentication.getName();
+
+        log.info("User: {}, Roles: {}", name, authorities);
     }
 
     @DeleteMapping("/{id}")
