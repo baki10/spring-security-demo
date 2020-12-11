b7be0e8afcdf1da06af62e28909a700a0634feb5
Ilmir Bakirov <bakirov@xrm.ru>
2020-12-11 10:58:58 +0500

    add some method security

--- jwt-token-demo/src/main/java/com/bakigoal/spring/service/RolesService.java
+++ jwt-token-demo/src/main/java/com/bakigoal/spring/service/RolesService.java
@@ -3,8 +3,14 @@ package com.bakigoal.spring.service;
 import com.bakigoal.spring.domain.Role;
 import com.bakigoal.spring.security.annotation.Allow;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RolesService {
@@ -19,8 +25,53 @@ public class RolesService {
         return "User data";
     }
 
+    @RolesAllowed({"ROLE_USER", "ROLE_ADMIN"})
+    public String rolesAllowed() {
+        return "User and Admin can see this data";
+    }
+
     @Allow(roles = {Role.ADMIN, Role.USER})
     public String getUserOrAdminData() {
         return "User and Admin can see this data";
+    }
+
+    @PreAuthorize("#name == authentication.principal.username")
+    public String getSecretNames(String name) {
+        return "Secret name: " + name;
+    }
+
+    /**
+     * @param products The list given as param allows only products owned by the authenticated user
+     */
+    @PreFilter("filterObject.owner == authentication.name")
+    public List<Product> sellProducts(List<Product> products) {
+        // sell products and return the sold product list
+        return products;
+    }
+
+    /**
+     * Adds the filtering for the objects in the collection returned by the method
+     */
+    @PostFilter("filterObject.owner == authentication.name")
+    public List<Product> findProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product().setOwner("baki"));
+        products.add(new Product().setOwner("aliya"));
+        products.add(new Product().setOwner("baki"));
+        products.add(new Product().setOwner("zombi"));
+        return products;
+    }
+
+    private static class Product {
+        private String owner;
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public Product setOwner(String owner) {
+            this.owner = owner;
+            return this;
+        }
     }
 }
